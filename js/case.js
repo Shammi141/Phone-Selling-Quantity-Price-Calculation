ce8f21eac24d4bb21967473395acc9f55e6a8f3f
Shammi141 <jannatulshammi181@gmail.com>
2022-08-19 00:21:48 +0600

    added stracture & phoneCase's quantity & price

---
+++ js/case.js
@@ -0,0 +1,42 @@
+/*Steps:------
+1. add event listener to the case plus button
+2.  get the value inside the case number field(input fiels)
+3.convert the number to an integer
+4. cakculate the new case number
+5. set the value to the new number field
+*/
+
+
+// phone's caseNumber plus, minus button function
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+    let newCaseNumber;
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+    caseNumberField.value = newCaseNumber;
+    return newCaseNumber;   //for price calculation
+}
+
+//update of case price function
+function updateCaseTotalPrice(newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+}
+
+//event haldler for plus, minus, price`
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(true);
+    updateCaseTotalPrice(newCaseNumber);
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(false);
+    updateCaseTotalPrice(newCaseNumber);
+});
